c3c6e1ab1ca07f7680dd25380557f35620fa13ed
DoubleFFWasTaken <filipfurenes@gmail.com>
2023-04-17 12:53:40 +0200

    endra

--- Hello_mannen/Test_1.py
+++ Hello_mannen/Test_1.py
@@ -1,6 +1,9 @@
-print("hello world")
+import matplotlib.pyplot as plt
 
-x = 0
-while x <= 11:
-    print(x*2)
-    x += 1
+x = [1,2,3,4,5,6,7,8,9,10]
+y = [2,4,6,8,10,12,14,16,18,20]
+
+plt.plot(x,y,label="gaming")
+plt.legend()
+plt.grid()
+plt.plot()
