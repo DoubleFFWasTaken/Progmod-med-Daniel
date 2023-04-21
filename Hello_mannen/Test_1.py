8c8e9e1bb7fc0df03dad17c43a49931105207e37
DoubleFFWasTaken <filipfurenes@gmail.com>
2023-04-21 09:20:44 +0200

    modified

--- Hello_mannen/Test_1.py
+++ Hello_mannen/Test_1.py
@@ -2,56 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from IPython.display import clear_output
-
-def getAcc( pos, mass, G, softening ):
-	# positions r = [x,y,z] for all particles
-	x = pos[:,0:1]
-	y = pos[:,1:2]
-	z = pos[:,2:3]
-
-	# matrix that stores all pairwise particle separations: r_j - r_i
-	dx = x.T - x
-	dy = y.T - y
-	dz = z.T - z
-
-	# matrix that stores 1/r^3 for all particle pairwise particle separations 
-	inv_r3 = (dx**2 + dy**2 + dz**2 + softening**2)
-	inv_r3[inv_r3>0] = inv_r3[inv_r3>0]**(-1.5)
-
-	ax = G * (dx * inv_r3) @ mass
-	ay = G * (dy * inv_r3) @ mass
-	az = G * (dz * inv_r3) @ mass
-	
-	# pack together the acceleration components
-	a = np.hstack((ax,ay,az))
-
-	return a
-
-def getEnergy( pos, vel, mass, G ):
-	
-	# Kinetic Energy:
-	KE = 0.5 * np.sum(np.sum( mass * vel**2 ))
-
-	# Potential Energy:
-
-	# positions r = [x,y,z] for all particles
-	x = pos[:,0:1]
-	y = pos[:,1:2]
-	z = pos[:,2:3]
-
-	# matrix that stores all pairwise particle separations: r_j - r_i
-	dx = x.T - x
-	dy = y.T - y
-	dz = z.T - z
-
-	# matrix that stores 1/r for all particle pairwise particle separations 
-	inv_r = np.sqrt(dx**2 + dy**2 + dz**2)
-	inv_r[inv_r>0] = 1.0/inv_r[inv_r>0]
-
-	# sum over upper triangle, to count each interaction only once
-	PE = G * np.sum(np.sum(np.triu(-(mass*mass.T)*inv_r,1)))
-	
-	return KE, PE;
+from getAcc import *
+from getEnergy import *
 
 def main():
 	
